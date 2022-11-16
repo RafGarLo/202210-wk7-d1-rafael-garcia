2775912d313fd575e8608ee2a0b5ef423b35eb04
Rafael <grafa74@gmail.com>
2022-11-16 17:10:02 +0100

    added changes to database

--- src/data/mongo.ts
+++ src/data/mongo.ts
@@ -1,10 +1,28 @@
 import * as dotenv from 'dotenv';
-dotenv.config();
+dotenv.config({path:'../../.env'});
 
-import mongoose from 'mongoose';
+import mongoose, { Schema, model } from 'mongoose';
+
 const uri = `mongodb+srv://${process.env.USER}${process.env.PASSWORD}${process.env.CLUSTER}`;
-const connector = async () => {
-  return await mongoose.connect(uri)
+console.log(uri);
+
+const main = async () => {
+    
+   const connector = await mongoose.connect(uri)
+   console.log(connector)
+   const Product = model('Product', productSchema, 'products');
+
+await Product.create({name:'chair', category: 'office', overview: 'very nice', price: '150'});
+
+connector.disconnect()
 }
-console.log(connector())
+
+
+const productSchema = new Schema ({
+
+    name: String,
+    category: String,
+    overview: String,
+    price: String,
+})
 
